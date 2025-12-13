6ec6c85fdf8dea06b2c72183507ee5415363d3ee
chinmay-meghare <chinmaym100@gmail.com>
2025-12-13 13:11:20 +0530

    fix: hide main navbar and footer on admin routes to prevent ui overlaps

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,18 @@ import Navbar from "./components/common/Navbar";
 import Footer from "./components/common/Footer";
 import AppRoutes from "./routes/AppRoutes";
 import CartInitializer from "./components/cart/CartInitializer";
+import { useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+  const isAdminRoute = location.pathname.startsWith("/admin");
+
   return (
     <>
       <CartInitializer />
-      <Navbar />
+      {!isAdminRoute && <Navbar />}
       <AppRoutes />
-      <Footer />
+      {!isAdminRoute && <Footer />}
     </>
   );
 }
